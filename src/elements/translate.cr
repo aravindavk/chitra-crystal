5345f837c26195b38b8c487c8a56f64c30de7c4a
Aravinda Vishwanathapura <mail@aravindavk.in>
2022-01-26 20:31:27 +0530

    Line Join and translate
    Signed-off-by: Aravinda Vishwanathapura <mail@aravindavk.in>

---
+++ src/elements/translate.cr
@@ -0,0 +1,30 @@
+module Chitra
+  struct Translate < Element
+    # :nodoc:
+    def initialize(@x : Float64, @y : Float64)
+    end
+
+    # :nodoc:
+    def draw(cairo_ctx)
+      cairo_ctx.translate @x, @y
+    end
+
+    # :nodoc:
+    def to_s
+      "translate(#{@x}, #{@y})"
+    end
+  end
+
+  class Context
+    # Translate the canvas to given x and y
+    # ```
+    # ctx = Chitra.new
+    # ctx.translate 100, 100
+    # ctx.rect 0, 0, 500, 500
+    # ```
+    def translate(x, y)
+      t = Translate.new(x, y)
+      @elements << t
+    end
+  end
+end
