f6ece623039f651228a02575b7ae18cd0aea0679
Aravinda Vishwanathapura <mail@aravindavk.in>
2022-01-27 11:38:39 +0530

    Added scale functionality
    Signed-off-by: Aravinda Vishwanathapura <mail@aravindavk.in>

--- src/global_context.cr
+++ src/global_context.cr
@@ -6,7 +6,7 @@ end
 
 FUNCS = %w[width height enable_debug fill no_fill stroke
   stroke_width save new_drawing rect oval line
-  line_dash line_cap line_join translate rotate
+  line_dash line_cap line_join translate rotate scale
 ]
 
 # Define the above global functions
