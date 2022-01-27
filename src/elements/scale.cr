5b74682d20ef551898196346b060410388a509b9
Aravinda Vishwanathapura <mail@aravindavk.in>
2022-01-27 11:38:39 +0530

    Added scale functionality
    Signed-off-by: Aravinda Vishwanathapura <mail@aravindavk.in>

---
+++ src/elements/scale.cr
@@ -0,0 +1,39 @@
+module Chitra
+  struct Scale < Element
+    # :nodoc:
+    def initialize(@scale_x : Float64, @scale_y : Float64 = 0.0)
+    end
+
+    # :nodoc:
+    def draw(cairo_ctx)
+      scale_y = @scale_y == 0 ? @scale_x : @scale_y
+      cairo_ctx.scale @scale_x, scale_y
+    end
+
+    # :nodoc:
+    def to_s
+      "scale(#{@scale_x}, #{@scale_y})"
+    end
+  end
+
+  class Context
+    # Scale the canvas with the given x and y scale factors.
+    # direction.
+    # ```
+    # ctx = Chitra.new 200
+    # ctx.scale 2
+    # ctx.rect 40, 40, 40, 40
+    # ```
+    # Different scaling for x and y
+    # ```
+    # ctx = Chitra.new 200
+    # ctx.scale 2, 1
+    # ctx.rect 40, 40, 40, 40
+    # ```
+    # Change the center of Scale
+    def scale(scale_x, scale_y = 0)
+      s = Scale.new(scale_x, scale_y)
+      @elements << s
+    end
+  end
+end
