cc93cd3c34413a3eee7af11bc32e7ccca3687dbf
Aravinda Vishwanathapura <mail@aravindavk.in>
2022-01-27 12:34:38 +0530

    Polygon shape added
    Signed-off-by: Aravinda Vishwanathapura <mail@aravindavk.in>

---
+++ src/elements/polygon.cr
@@ -0,0 +1,38 @@
+module Chitra
+  struct Polygon < Element
+    include ShapeProperties
+
+    # :nodoc:
+    def initialize(@points : Array(Array(Float64)), @close = true)
+    end
+
+    # :nodoc:
+    def draw(cairo_ctx)
+      cairo_ctx.move_to(@points[0][0], @points[0][1])
+      @points[1..].each do |p|
+        cairo_ctx.line_to(p[0], p[1])
+      end
+      cairo_ctx.close_path if @close
+      draw_shape_properties(cairo_ctx)
+    end
+
+    # :nodoc:
+    def to_s
+      debug_text "polygon(#{@points.join(",")}, close=#{@close})"
+    end
+  end
+
+  class Context
+    # Draw polygon shape. By default closes the path
+    # ```
+    # ctx = Chitra.new
+    # x y w h
+    # ctx.polygon 50, 450, 50, 50, 450, 50, 100, 100, close: true
+    # ```
+    def polygon(*points, close = true)
+      values = coord_pairs_validate(points)
+      p = Polygon.new(values, close)
+      add_shape_properties(p)
+    end
+  end
+end
