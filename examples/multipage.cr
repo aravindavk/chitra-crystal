50959de6ab29227960d9bb29ac2ed8ae649410eb
Aravinda Vishwanathapura <mail@aravindavk.in>
2022-05-03 22:37:43 +0530

    Added support for multipage PDF documents (#20)
    Signed-off-by: Aravinda Vishwanathapura <mail@aravindavk.in>

---
+++ examples/multipage.cr
@@ -0,0 +1,19 @@
+require "../src/global_context"
+
+# A4 Page size
+size 2480, 3508
+
+fill 1, 0, 0
+rect 500, 500, 500, 500
+
+new_page
+
+fill 0, 1, 0
+rect 500, 500, 500, 500
+
+new_page
+
+fill 0, 0, 1
+rect 500, 500, 500, 500
+
+save "multipage.pdf"
