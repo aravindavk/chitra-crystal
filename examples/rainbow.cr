1898c2544107f783530d9c932d9f3c3973d5db33
Aravinda Vishwanathapura <mail@aravindavk.in>
2022-08-16 11:56:29 +0530

    Add support for Join String for Rainbow text
    Signed-off-by: Aravinda Vishwanathapura <mail@aravindavk.in>

--- examples/rainbow.cr
+++ examples/rainbow.cr
@@ -7,7 +7,7 @@ fill 0
 no_stroke
 rainbow "Hello World".split(""), 20, 20
 
-rainbow "This$ is$ colorful$ text.$ How$ is$ it?".split("$"), 20, 100
+rainbow "This is colorful text. How is it?".split(" "), 20, 100, join_str: " "
 
 rainbow "Awesome, nice and nice".split(""), 20, 180
 
@@ -16,6 +16,6 @@ rainbow "rainbow colors", 20, 260
 rainbow "ಒತ್ತಕ್ಷರ", 20, 340
 rainbow "Made in India", 20, 430, colors: ["#FF9933", "#FFFFFF", "#138808"]
 
-rainbow "Kannada ಕನ್ನಡ", 20, 500, colors: ["yellow", "red"]
+rainbow %w[k a n n a d a ಕ ನ್ನ ಡ], 20, 500, colors: ["yellow", "red"]
 
 save "rainbow.png"
