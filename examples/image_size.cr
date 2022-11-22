a02422e29837bd883ae743847aed76da0adb920f
Aravinda Vishwanathapura <mail@aravindavk.in>
2022-11-22 16:33:32 +0530

    image size API
    Find image width or height or both together.
    
    ```crystal
    w = image_width "poster.png"
    h = image_height "poster.png"
    w, h = image_size "poster.png"
    ```
    
    Calling image_width or height or size multiple times is
    optimized by caching the image surface once.
    
    Signed-off-by: Aravinda Vishwanathapura <mail@aravindavk.in>

---
+++ examples/image_size.cr
@@ -0,0 +1,6 @@
+require "../src/global_context"
+
+image_file = "../docs/content/static/logo.png"
+puts image_width image_file
+puts image_height image_file
+puts image_size image_file
--- src/elements/image.cr
+++ src/elements/image.cr
@@ -6,7 +6,7 @@ module Chitra
 
     # :nodoc:
     def draw(cairo_ctx)
-      surface = LibCairo.cairo_image_surface_create_from_png(@path.to_unsafe)
+      surface = Chitra.image_surface(@path)
       LibCairo.cairo_set_source_surface(cairo_ctx, surface, @x, @y)
       LibCairo.cairo_paint(cairo_ctx)
     end
@@ -28,6 +28,35 @@ module Chitra
       idx = add_shape_properties(i)
 
       draw_on_default_surface(@elements[idx])
+    end
+
+    # Get width of the image
+    # ```
+    # ctx = Chitra.new
+    # w = ctx.image_width "logo.png"
+    # ```
+    def image_width(path)
+      surface = Chitra.image_surface(path)
+      LibCairo.cairo_image_surface_get_width(surface)
+    end
+
+    # Get height of the image
+    # ```
+    # ctx = Chitra.new
+    # h = ctx.image_height "logo.png"
+    # ```
+    def image_height(path)
+      surface = Chitra.image_surface(path)
+      LibCairo.cairo_image_surface_get_height(surface)
+    end
+
+    # Get width and height of the image
+    # ```
+    # ctx = Chitra.new
+    # w, h = ctx.image_size "logo.png"
+    # ```
+    def image_size(path)
+      {image_width(path), image_height(path)}
     end
   end
 end
